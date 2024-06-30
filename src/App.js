1a7f0865944c03f4801fcd39862874aac9a86029
Murat Yavuzturk <myavuzturk51@gmail.com>
2024-06-30 14:48:37 -0700

    updated app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Murat's Linkedin Bio.
+          Welcome to Murat's Linkedin Bio.
         </p>
         <a
           className="App-link"
